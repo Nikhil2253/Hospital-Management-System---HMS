f9062990871ed2715b3ffc06b9d47034a63bfea3
Nikhil2253 <147965031+Nikhil2253@users.noreply.github.com>
2025-04-08 13:03:14 +0530

    Create App.jsx

---
+++ frontend/src/App.jsx
@@ -0,0 +1,19 @@
+import Footer from "./components/Footer"
+import Hero from "./components/Hero"
+import Navbar from "./components/Navbar"
+import Showcase from "./components/Show"
+import "./index.css"
+
+function App() {
+
+  return (
+    <>
+      <Navbar/>
+      <Hero/>
+      <Showcase/>
+      <Footer/>
+    </>
+  )
+}
+
+export default App
